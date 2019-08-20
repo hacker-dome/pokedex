228771780b7db0bc2c3a78965f8bea556fd852f7
Pierre Guillen <hi@cerbero.dev>
2019-08-20 00:55:50 -0500

    Añadiendo splash pokedex

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,46 @@ import 'package:flutter/material.dart';
 //import 'package:pokedex/src/page/hijo_page.dart';
 //import 'package:pokedex/src/page/login_page.dart';
 import 'package:pokedex/src/pokedex/pokedex_screen.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 
 
-void main() => runApp(MainApp());
+void main() => runApp(
+    new MaterialApp(
+      home:MainApp(),
+      ));
+
+    
+    
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 5,
+        navigateAfterSeconds: new MainApp(),
+        title: new Text('Welcome In SplashScreen',
+          style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0
+          ),),
+
+        image: new Image.asset('assets/pokedex.png'),
+
+        backgroundColor: Colors.white70,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: ()=>print("Cerbero Dev"),
+        loaderColor: Colors.black45
+    );
+  }
+}
+
 
 class MainApp extends StatelessWidget {
 
